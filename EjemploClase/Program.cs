778d27a7b882cfd8bde2a57494e6c264c48b76d5
SanchezPedro110U <i19222006.19@puebla.tecnm.mx>
2022-02-15 13:47:05 -0600

    primer repositorio

---
+++ EjemploClase/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace EjemploClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Pedro Luis";
+            string lastname = "Sánchez Arce";
+            //Escribir una sola línea con Write
+            Console.Write("Pedro Luis");
+            Console.Write("Sánchez Arce");
+            Console.Write("No.Control");
+
+            //Escribir con saltos de Línea
+            Console.WriteLine("Pedro Luís");
+            Console.WriteLine("Sánchez Arce");
+            Console.WriteLine("No.Control");
+            Console.WriteLine("19222006");
+
+            //Escribir saltos de línea pero con tipos de datos impresación
+            Console.WriteLine("Hola " + name);
+            Console.WriteLine("Hola " + lastname);
+
+            //Escribir por medio de RedLine con WriteLine
+            Console.WriteLine("Hola , ingresa tu nombre");
+            Console.WriteLine("Hola " + Console.ReadLine()+" bienvenido");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
